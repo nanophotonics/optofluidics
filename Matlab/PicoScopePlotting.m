719fb6bc7efe0eb8fb4dd290eebfbd13055dfb15
anaandresarroyo <aa938@cam.ac.uk>
2016-06-17 17:16:16 +0100

    Improvements
    Improvements

--- Matlab/PicoScopePlotting.m
+++ Matlab/PicoScopePlotting.m
@@ -6,7 +6,7 @@ close all
 % *************************************************************************
 
 % select default path
-file_path = 'C:\Users\Ana Andres\Documents\NanoPhotonics\Laboratory\';
+file_path = 'R:\aa938\Laboratory\2016-06-17 - 60 nm Au\';
 
 % pop up window to choose the file(s) to read
 [file_name, file_path, ~] = uigetfile('.txt',...
@@ -27,27 +27,41 @@ number_of_files = size(file_name,2);
 % it takes a while because the files are large
 raw_data = cell(size(file_name));
 for i = 1:1:number_of_files
+    disp(['Reading File ' num2str(i) '/' num2str(number_of_files)])
     raw_data{i} = dlmread([file_path file_name{i}], '\t',2,0);
     % raw_data{i}(:,1) = time
     % raw_data{i}(:,2) = channel A
     % raw_data{i}(:,3) = channel B
 end
 
-% plotting the data
+%% PLOTTING DATA
+% *************************************************************************
+close all
 figure_picoscope = figure('Units','normalized','Position',[0.01 0.1 0.97 0.64],'tag','figure_picoscope');
 hold all
 channel_name = {'Time', 'Channel A', 'Channel B', 'Channel C', 'Channel D'};
+channel_factor = [1,1,1];
+measurement_factor = [26,1,26,28,22,1,1,1,1,1,1];
+centre_wavelength = [592, 592, 676.2, 744.5, 831.0];
+smoothing_points = 1;
 legend_picoscope = {}; % initialising the legend cell
 for i = 1:1:number_of_files % files
-    for j = 2:1:size(raw_data{i},2) % channels
-        plot(raw_data{i}(:,1), raw_data{i}(:,j))
-        legend_picoscope{end+1} = [file_name{i}(1:end-4) ' // ' channel_name{j}];
+%     for j = 2:1:size(raw_data{i},2) % channels
+    for j = 3 % channels
+        plot(raw_data{i}(:,1), ...
+            smooth(raw_data{i}(:,j),smoothing_points)*channel_factor(j)*measurement_factor(i),...
+            'LineWidth', 2)
+        legend_picoscope{end+1} = [file_name{i}(1:end-4) ' // ' ...
+            channel_name{j} ' x ' num2str(channel_factor(j)*measurement_factor(i), '%02.0f')...
+            ' // smoothing = ' num2str(smoothing_points) ' points'...
+            ' // centre = ' num2str(centre_wavelength(i), '%03.1f') ' nm'];
     end
 end
 grid on
-legend(legend_picoscope, 'Location', 'best')
+legend(legend_picoscope, 'Location', 'best', 'interpreter', 'none')
 title(file_path, 'interpreter', 'none')
 xlabel('Time (ms)')
-ylabel('Signal (mV)')
+ylabel('Signal (V)')
+xlim([-50,250])
 
 
---
+++ Python/HDF5_SpectrumAnalyser.py
@@ -0,0 +1,184 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 09 15:26:26 2016
+
+@author: Ana Andres
+"""
+
+#from PyQt4.uic import loadUiType
+
+from nplab.utils.gui import *
+from nplab.utils.notified_property import DumbNotifiedProperty
+from nplab.ui.hdf5_browser import *
+from PyQt4 import uic
+from PyQt4 import QtGui, QtCore #TODO: I think these should be wrapped by nplab.utils.gui? rwb
+
+
+import numpy as np
+import h5py
+
+import matplotlib
+import matplotlib.pyplot as plt
+#matplotlib.use('Qt4Agg')
+#from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
+#from matplotlib.figure import Figure
+#from nplab.ui.data_renderers import suitable_renderers
+from nplab.ui.ui_tools import UiTools
+
+import os
+
+
+class HDF5SpectrumAnalyser(QtGui.QMainWindow, UiTools):
+    """
+    GUI which analyses the timelapse spectra recorded with Spectrometerself.timelapse.py    
+    """
+    def __init__(self, data_file, parent=None):
+        super(HDF5SpectrumAnalyser, self).__init__(parent)
+        self.data_file = data_file
+        uic.loadUi(os.path.join(os.path.dirname(__file__), 'HDF5_SpectrumAnalyser_GUI.ui'), self)
+        
+        self.tree = HDF5Tree(self.data_file,
+                             parent=self,
+                             treeWidget=self.FileContentsTreeWidget, 
+                             refresh_button=self.RefreshTreePushButton,
+                             )
+        self.tree.treeWidget.itemClicked.connect(self.on_click)
+        
+        self.NewFilePushButton.clicked.connect(self.new_file)
+                
+        self.viewer = HDF5ItemViewer(parent=self, 
+                                     figure_widget=self.RendererFigureWidget,
+                                     show_controls=False, 
+                                     renderer_combobox = self.HDF5RendererComboBox,
+                                     default_button=self.DefaultRendererPushButton,
+                                     refresh_button=self.RefreshFigurePushButton,
+                                     copy_button=self.CopyFigurePushButton,
+                                     )             
+        self.replace_widget(self.RendererWidgetContainer, self.RendererFigureWidget, self.viewer)
+        
+        self.AnalysePlotPushButton.clicked.connect(self.analyse_and_plot_data)
+        
+        """Populate Default Combobox Options"""
+        
+#        self.BackgroundComboBox.addItem('None', 0)
+#        self.ReferenceComboBox.addItem('None', 0)
+        self.NormalisationComboBox.addItem('None', 0)
+        
+        self.TwoDPlotColourComboBox.addItem('Default Colours', 0)        
+        self.SpectraColourComboBox.addItem('Default Colours', 0)        
+        self.TraceColourComboBox.addItem('Default Colours', 0)        
+        
+        self.TwoDPlotFigureComboBox.addItem('New Figure', 0)
+        self.SpectraFigureComboBox.addItem('New Figure', 0)
+        self.TraceFigureComboBox.addItem('New Figure', 0)
+        
+        self.auto_connect_by_name()
+    
+    """"Connecting the checkboxes to the properties"""
+    TwoDPlot = DumbNotifiedProperty()
+    Spectra = DumbNotifiedProperty()
+    Trace = DumbNotifiedProperty()
+        
+    
+    def on_click(self, item, column):        
+        """Handle clicks on items in the tree."""
+#        item.setExpanded(True)
+        if len(self.tree.treeWidget.selectedItems())>1: 
+#            self.viewer.data = [treeitem.data(column, QtCore.Qt.UserRole) for treeitem in self.tree.treeWidget.selectedItems() ]
+            self.viewer.data = DummyHDF5Group({treeitem.data(column, QtCore.Qt.UserRole).name : treeitem.data(column, QtCore.Qt.UserRole) \
+                                                for treeitem in self.tree.treeWidget.selectedItems() })
+        else:
+            self.viewer.data = item.data(column, QtCore.Qt.UserRole)
+            self.item_names = self.viewer.data.name
+#            print self.viewer.data.name
+
+            self.timelapse = self.viewer.data
+            self.Spectra = self.timelapse.items()
+    
+            raw = []
+            self.spectra_name = []
+            for Spectrum in self.Spectra:
+                raw.append(np.array(self.timelapse.get(Spectrum[0])))
+                self.spectra_name.append(Spectrum[0])
+            self.intensity = raw            
+            
+            self.attributes = self.timelapse.get(self.Spectra[0][0]).attrs.items()
+            
+            """ Repopulate the Comboboxes"""
+            # only checks the attributes of the last one: fix this!
+            self.BackgroundComboBox.clear()
+            self.BackgroundComboBox.addItem('None', 0)
+            self.ReferenceComboBox.clear()
+            self.ReferenceComboBox.addItem('None', 0)     
+            self.BackgroundComboBox.addItem('From File', 3)
+            self.ReferenceComboBox.addItem('From File', 3)
+            for attribute in self.attributes:
+                if attribute[0] == 'background':
+                    self.BackgroundComboBox.addItem('From Metadata', 3)
+                if attribute[0] == 'reference':
+                    self.ReferenceComboBox.addItem('From Metadata', 3)                
+                    
+                        
+        
+    def new_file(self):
+        """Select a new HDF5 file and populate the file contents tree widget."""
+        self.data_file.close()
+        self.data_file = nplab.datafile.open_file()
+        self.tree.data_file = self.data_file
+        self.tree.refresh()
+    
+    def analyse_and_plot_data(self):
+        
+        """
+        This is reading the attributes of the last spectra only.
+        We assume that all spectra have the same attributes
+        """
+        
+        for attribute in self.attributes:
+        #    print attribute[0]
+            if attribute[0] == 'wavelengths':
+#                print 'Read Wavelengths'
+                Wavelengths = attribute[1]
+#            if attribute[0] == 'reference':
+#                print 'Read Reference'
+#                Reference = attribute[1]
+#            if attribute[0] == 'background':
+#                print 'Read Background'
+#                Background = attribute[1]
+            if attribute[0] == 'information':
+#                print 'Read Information'
+                Information = attribute[1]     
+        
+        plt.rcParams.update({'font.size': 32})
+        
+        plt.figure(figsize=[25,15])
+        
+        
+        AxisSpectra = plt.subplot(111)
+        PlotsSpectra = []
+        for s in range(0,len(self.intensity),1):
+            PlotsSpectra.append(
+                               AxisSpectra.plot(
+                                               Wavelengths,self.intensity[s], 
+                                               '-', 
+                                               label=self.spectra_name[s], 
+                                               linewidth=4.0,
+                                               )
+                               )
+                               
+        plt.xlabel('Wavelength (nm)')
+        plt.ylabel('Intensity (a.u.)')
+        plt.title(self.timelapse.name + ' // ' + Information)
+        AxisSpectra.legend(numpoints = 1, loc='upper right')
+
+
+if __name__ == '__main__':
+    import sys, h5py, os, numpy as np
+    import nplab
+    
+    print os.getcwd()
+    app = get_qt_app()
+    data_file = h5py.File('C:/Users/Ana Andres/Documents/Python Scripts/2016-05-17.h5', 'r')
+#    data_file = nplab.datafile.open_file()
+    ui = HDF5SpectrumAnalyser(data_file)
+    ui.show()
