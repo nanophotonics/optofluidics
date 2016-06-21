1f386fac0f49aa075e80fe0d10a994462c39916c
anaandresarroyo <aa938@cam.ac.uk>
2016-06-21 10:21:52 +0100

    Savitzky-Golay filtering
    Savitzky-Golay filtering implementation

--- Matlab/PicoScopePlotting.m
+++ Matlab/PicoScopePlotting.m
@@ -6,11 +6,17 @@ close all
 % *************************************************************************
 
 % select default path
-file_path = 'R:\aa938\Laboratory\2016-06-17 - 60 nm Au\';
+file_path = 'C:\Users\Ana Andres\Documents\NanoPhotonics\Laboratory\2016.06.17 - 60 nm Au\';
 
 % pop up window to choose the file(s) to read
 [file_name, file_path, ~] = uigetfile('.txt',...
     'PicoScope Fil to Read (use CTRL to select multiple files)',file_path,'MultiSelect','on');
+if strfind(file_path, 'Laboratory')
+    title_cell{1} = file_path(strfind(file_path, 'Laboratory')+11:end);
+else
+    title_cell{1} = file_path;
+end
+    
 
 % if just a single file is selected, change the variable file_names
 % from a string to a cell so the loops indexing will work
@@ -34,32 +40,56 @@ for i = 1:1:number_of_files
     % raw_data{i}(:,3) = channel B
 end
 
+%% SMOOTHING DATA
+% *************************************************************************
+
+menu_smoothing = 3;
+menu_smoothing = menu('Smooth Data?', 'NO', 'YES: Savitzky-Golay filtering');
+
+smoothed_data = raw_data;   
+title_cell{2} = 'Raw unfiltered data';
+if menu_smoothing == 2 % Savitzky-Golay filtering
+    % if polynomial_order = 1 then this becomes a moving average
+    polynomial_order = 4; % must be less than the frame size
+    frame_size = 2001; % must be odd
+    title_cell{2} = ['Savitzky-Golay filtering.'...
+        ' Polynomial order = ' num2str(polynomial_order)...
+        ' Frame size =  ' num2str(frame_size) ' points.'];
+    
+    for i = 1:1:number_of_files % files
+        disp(['Smoothing File ' num2str(i) '/' num2str(number_of_files)])
+        for j = 2:1:size(smoothed_data{i},2) % channels
+            smoothed_data{i}(:,j) = sgolayfilt(raw_data{i}(:,j),polynomial_order,frame_size);
+        end
+    end
+end
+
+
 %% PLOTTING DATA
 % *************************************************************************
-close all
+% close all
 figure_picoscope = figure('Units','normalized','Position',[0.01 0.1 0.97 0.64],'tag','figure_picoscope');
 hold all
 channel_name = {'Time', 'Channel A', 'Channel B', 'Channel C', 'Channel D'};
 channel_factor = [1,1,1];
 measurement_factor = [26,1,26,28,22,1,1,1,1,1,1];
 centre_wavelength = [592, 592, 676.2, 744.5, 831.0];
-smoothing_points = 1;
+
 legend_picoscope = {}; % initialising the legend cell
 for i = 1:1:number_of_files % files
-%     for j = 2:1:size(raw_data{i},2) % channels
+%     for j = 2:1:size(smoothed_data{i},2) % channels
     for j = 3 % channels
-        plot(raw_data{i}(:,1), ...
-            smooth(raw_data{i}(:,j),smoothing_points)*channel_factor(j)*measurement_factor(i),...
+        plot(smoothed_data{i}(:,1), ...
+            smoothed_data{i}(:,j)*channel_factor(j)*measurement_factor(i),...
             'LineWidth', 2)
         legend_picoscope{end+1} = [file_name{i}(1:end-4) ' // ' ...
             channel_name{j} ' x ' num2str(channel_factor(j)*measurement_factor(i), '%02.0f')...
-            ' // smoothing = ' num2str(smoothing_points) ' points'...
             ' // centre = ' num2str(centre_wavelength(i), '%03.1f') ' nm'];
     end
 end
 grid on
 legend(legend_picoscope, 'Location', 'best', 'interpreter', 'none')
-title(file_path, 'interpreter', 'none')
+title(title_cell, 'interpreter', 'none')
 xlabel('Time (ms)')
 ylabel('Signal (V)')
 xlim([-50,250])
