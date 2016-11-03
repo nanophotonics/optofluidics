2f6d9dbcc8502c3f5cc9e51e091e749b598879f6
anaandresarroyo <aa938@cam.ac.uk>
2016-11-03 13:46:19 +0000

    Fibre Transmission
    Calculation of the transmission through the fibre for a given medium,
    wavelength, and fibre length.

--- Matlab/TiSaPowerCalculation.m
+++ Matlab/TiSaPowerCalculation.m
@@ -23,7 +23,6 @@ input_data = {'Reference Ange (deg):',...
               'Laser Wavelength (nm):', ...
               'Medium:', ...
               };
-resize = 'on'; dimensions = [1 60];
 default_values = {num2str(reference_angle),...
                   num2str(reference_input_power),...
                   num2str(reference_output_power),...
@@ -31,15 +30,19 @@ default_values = {num2str(reference_angle),...
                   num2str(wavelength),...
                   medium,...
                   };
-answer = inputdlg(input_data, input_title, dimensions, default_values, resize);
+dlg_options.WindowStyle = 'normal'; dlg_options.Resize = 'on'; dim = [1 60];
+answer = inputdlg(input_data, input_title, dim, default_values, dlg_options);
 reference_angle = str2double(answer{1});   
 reference_input_power = str2double(answer{2});   
 reference_output_power = str2double(answer{3});   
 fibre_length = str2double(answer{4});   
 wavelength = str2double(answer{5});   
 medium = answer{6};   
-    
-fibre_attenuation = absorption(medium, wavelength); % dB/m. Just includes the H2O/D2O absorption.
+
+fibre_attenuation = absorption(medium, wavelength); % dB/m. 
+% Just includes the H2O/D2O absorption and no other losses in the fibre!!!
+% The total fibre attenuation should be measured experimentally with the cutback method.  
+
 reference_in_coupled_power = reference_output_power * 10^ (fibre_attenuation * (fibre_length / 100) / 10); % mW
 in_coupling_transmission = reference_in_coupled_power / reference_input_power * 100; % percentage
 fibre_transmission = reference_output_power / reference_in_coupled_power * 100; % percentage
