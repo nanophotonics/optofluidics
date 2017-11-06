76ce9da66a1d168895fff153090e5700f123ec99
ChrisGrosse <cg632@cam.ac.uk>
2017-11-06 11:33:41 +0000

    various fixes fast analgue Arduino read, now optimized for reading only one ADC channel

---
+++ Arduino/FastAnalogueReadout_1Channel/FastAnalogueReadout_1Channel.ino
@@ -0,0 +1,244 @@
+
+// This programm enables the fast reading the analogue input of
+// an Arduino Due after a given delay of a trigger send to one
+// of its digital I/O pins.
+// The communication with a PC is done via the native USB port of an
+// Arduino Due using the CmdMessenger v4.0 libary written by Neil Dudman,
+// Dreamcat4, Thijs Elenbaas and Valeriy Kucherenko
+// (https://github.com/thijse/Arduino-CmdMessenger).
+
+#include <CmdMessenger.h>  // sending/receiving user-defined commands
+
+// global variables
+// ================
+int input_res = 4096;
+float input_max = 3.3;
+float input_min = 0;
+
+float read_delay = 37e-6;
+bool record_intensity = true;
+
+// get class instances for serial communication with PC
+CmdMessenger cmd = CmdMessenger(SerialUSB, xmodem);
+
+// ==========================================================
+// Initialisation of CmdMessenger and definition of commands
+// ==========================================================
+
+// definition of supported commands
+enum
+{
+  check_arduino,
+  ret_fast_intensity,
+  sweep_delay,
+  set_delay,
+  get_delay,
+  get_intensity_trace,
+  oscilloscope_mode,
+  ret_string,              // returns a string
+  ret_int,                 // returns an integer
+  ret_char,                // returns a character
+  ret_float,               // returns a float value
+  ret_double,              // returns a double value
+  ret_bool,                // returns a boolean value
+  ret_byte                 // returns a byte value
+};
+
+// callback methods for commands
+void attachCommands()
+{
+  cmd.attach(UnknownCommand);
+  cmd.attach(check_arduino, on_check_arduino);
+  cmd.attach(sweep_delay, on_sweep_delay);
+  cmd.attach(set_delay, on_set_delay);
+  cmd.attach(get_delay, on_get_delay);
+  cmd.attach(get_intensity_trace, on_get_intensity_trace);
+  cmd.attach(oscilloscope_mode, on_oscilloscope_mode);
+}
+
+// ==============================
+// CmdMessenger callback methods
+// ==============================
+
+void UnknownCommand() {
+  cmd.sendCmd(ret_string, "Error: Command without attached callback!");
+}
+
+void on_check_arduino() {
+  cmd.sendCmd(ret_string, "Arduino communication is fine.");
+}
+
+void on_set_delay() {  
+  read_delay = cmd.readBinArg<float>();
+}
+void on_get_delay() {
+  cmd.sendBinCmd(ret_float, read_delay);  
+}
+
+void on_sweep_delay() {  
+  int data_points = cmd.readBinArg<int>();
+  int num_pulses = cmd.readBinArg<int>();
+  float delay_increment = cmd.readBinArg<float>();
+  
+  float pulse_off = 5e-3; // in seconds 
+  float curr_sig1;
+  float curr_delay;
+  uint32_t RA;
+  uint32_t RC;
+  uint32_t clock_counts;
+
+  for (int i = 0; i < data_points; i++) {
+    curr_sig1 = 0;
+    curr_delay = 0;    
+    // set register A of channel 0 of Timer 1 to number of clock periods that correspond to current delay
+    RA = (i+1)*delay_increment/23.8095e-9;
+    TC_SetRA(TC1,0, RA); 
+    // set register C to number of clock periods when (trigger) pulse should be off
+    RC = pulse_off/23.8095e-9;
+//    TC_SetRC(TC1,0, RC);
+
+    for (int avg_no = 0; avg_no < num_pulses; avg_no++) {         
+      curr_sig1 += (float(delayed_ADC_read()) / (input_res - 1)) * (input_max - input_min) / num_pulses;
+      clock_counts = REG_TC1_CV0;         // read counter (was stopped after ADC conversion finished)
+      curr_delay += clock_counts/num_pulses*23.8095e-9;           
+//      while ((REG_TC1_SR0 & 0x10) == 0); // check TC status register for occurance of RC compare
+      delay(4);  
+    }
+    // send (averaged) intensity to PC
+    cmd.sendCmdStart(ret_fast_intensity);
+    cmd.sendCmdBinArg(curr_sig1);
+    cmd.sendCmdBinArg(curr_delay);
+    cmd.sendCmdEnd();
+  }
+}
+
+void on_get_intensity_trace() {
+
+  int data_points = cmd.readBinArg<int>();
+  int num_pulses = cmd.readBinArg<int>();
+  float delay_increment = cmd.readBinArg<float>();  // in seconds
+  
+  float curr_sig1;
+  float curr_delay;
+  uint32_t RA;
+  uint32_t RC;
+  uint32_t clock_counts;
+
+  for (int i = 0; i < data_points; i++) {
+    curr_sig1 = 0;
+    curr_delay = 0;
+    
+    // set register A of channel 0 of Timer 1 to number of clock periods that correspond to current delay
+    RA = read_delay/23.8095e-9;
+    TC_SetRA(TC1,0, RA); 
+    // set register C to number of clock periods when (trigger) pulse should be off
+//    RC = pulse_off/23.8095e-9;
+//    TC_SetRC(TC1,0, RC);
+
+    for (int avg_no = 0; avg_no < num_pulses; avg_no++) {         
+      curr_sig1 += (float(delayed_ADC_read()) / (input_res - 1)) * (input_max - input_min) / num_pulses;
+//      clock_counts = REG_TC1_CV0;         // read counter (was stopped after ADC conversion finished)
+//      curr_delay += clock_counts/avg_points*23.8095e-9;           
+//      while ((REG_TC1_SR0 & 0x10) == 0); // check TC status register for occurance of RC compare
+      delay(4);  
+    }    
+    // send (averaged) intensity to PC
+    cmd.sendCmdStart(ret_fast_intensity);
+    cmd.sendCmdBinArg(curr_sig1);
+    cmd.sendCmdBinArg(curr_delay);
+    cmd.sendCmdEnd();
+    delay(delay_increment*1000);    // wait until next intensity measurement
+  } 
+}
+
+int delayed_ADC_read() {
+
+  int intensity1_level;
+  uint32_t counter_status; 
+
+  REG_PIOB_SODR |= (1<<25);           // emit trigger by set pin 2 HIGH (port B, pin 25)     
+  counter_status = REG_TC1_SR0;       // clear status registers by reading them   
+  counter_status = REG_TC1_CV0;        
+  REG_TC1_CCR0 = 0x5;                 // reset and enable counter of channel 0 of timer 1      
+  while ((REG_TC1_SR0 & 0x4) == 0);   // wait until RA compare by checking TC status register
+  ADC->ADC_CR = 0x2;                  // start ADC conversion   
+  while ((ADC->ADC_ISR & 0x80) == 0); // wait until AD conversion of channel 7 finished by checking Interrupts Status Register (ADC_ISC)
+  intensity1_level = ADC->ADC_CDR[7]; //get values from Channel Data Register (ADC_CDR) of channel 7  
+  REG_TC1_CCR0 = 0x2;                 // disable counter
+  REG_PIOB_CODR |= (1<<25);           // set pin 2 LOW (port B, pin 25)
+  return intensity1_level;  
+}
+
+
+/*
+   reads the ADCs as fast as possible with the
+   specified number of data points
+*/
+void on_oscilloscope_mode() {
+  
+  int data_points = cmd.readBinArg<int>();
+  uint32_t clock_counts[data_points];
+  float signal1[data_points];
+  float time_delay;
+
+  REG_TC1_CCR0 = 0x5;                     // reset and enable counter of channel 0 of timer 1   
+  for (int i = 0; i < data_points; i++) {
+    ADC->ADC_CR |= (1<<1);                // start ADC conversion    
+    while ((ADC->ADC_ISR & 0x80) == 0);   // check Interrupts Status Register (ADC_ISC) for channel 7 (A0) and wait for conversion
+    signal1[i] = ADC->ADC_CDR[7];         //get values from Channel Data Register (ADC_CDR) of channel 7
+    clock_counts[i] = REG_TC1_CV0;        // read timer counter 
+  }
+
+  for (int i = 0; i < data_points; i++) {
+    time_delay = clock_counts[i]*23.8095e-9;
+    cmd.sendCmdStart(ret_fast_intensity);  
+    cmd.sendCmdBinArg(signal1[i]);
+    cmd.sendCmdBinArg(time_delay);
+    cmd.sendCmdEnd();
+  }  
+}
+
+
+void setup()
+{
+  // setup pins
+  pinMode(A0, INPUT); // ADC
+  pinMode(2, OUTPUT); // trigger pin
+
+  // set up Timer 1, channel 0 (ID_TC3) for delayed ADC measurement
+  pmc_enable_periph_clk(TC3_IRQn); 
+  REG_TC1_CMR0= 0x00008040; // set channel mode register (use waveform mode, MCL/2 clock (42 MHz), no effects on triggers, TIOA, and TIOB, timer stopped when it reaches RC)
+  REG_TC1_IDR0= 0xFF; // disable all interrupts
+
+  // set the ADC registers
+  ADC->ADC_CHER = 0x80;       // enable ADC on pin A0 (corresponds to Ch7 on Atmel chip)
+  //ADC->ADC_MR = 0x90330180;
+  ADC->ADC_MR = 0x10000180;   // set the ADC Mode Register (ADC_MR)
+  // default value: 0x9038ff00, binary: 0000 0000 1001 0000 0011 1000 1111 1111 0000 0000  
+  // structure of register:
+  //
+  // 0000 0000    0  0    01      0000     0   0    00     0000  0000 0001   1     000 0000
+  //        -- |USEQ|-|transfer|tracking|ANACH|-|settling|startup|prescale|freerun|..  
+  //
+  // this uses the following prameters:
+  // 
+  // * use free run mode, that is continuouse AD conversion without trigger
+  // * prescale for ADC clock: 1 => 84 MHz/2/(1+1) = 21 MHz -> ADC clock period: 47.6 ns
+  // * startup time: 0 ADC clock periods (time befor starting first conversion, only relevant when sleep mode enabled)
+  // * settling time: 3 ADC clock periods (higher values only necessay when changing offset or gain between conversion of different channels)
+  // * ANACH: 0 (no differnt analog settings for different ADC channels allowed)
+  // * tracking time: 0 (set bits don't have any effect because of a hardware defect, tracking time is always 15 ACD clock periods, which is the time for one AD conversion)
+  // * transfer time: 1 -> corresponds 5 to ADC clock periods, determines time between change of ADC channels and the corresponds to the time it takes to transfer the analog input from the sample to hold
+  // * USEQ: 0 -> no user-defined sequence for ADC channels because just one channel 
+  
+  SerialUSB.begin(115200); // initialise USB port, actual baud rate irrelevant as always USB 2.0 speed
+  attachCommands();       // attach CmdMessenger commands
+}
+
+void loop()
+{
+  // process incoming serial data from PC and perform callbacks
+  cmd.feedinSerialData();
+}
+
+
--- Python/Arduino interface/arduino_due_optofluidics.py
+++ Python/Arduino interface/arduino_due_optofluidics.py
@@ -3,25 +3,25 @@ __author__ = 'chrisgrosse'
 
 from PyCmdMessenger import CmdMessengerThreaded, ArduinoDueBoard
 
+import numpy as np
 import pyqtgraph as pg
 from collections import deque
 
 from nplab.instrument import Instrument
 from nplab.utils.gui import QtWidgets, get_qt_app, QtCore
+from nplab import datafile
 
 
 class ArduinoDueOptofluidics(CmdMessengerThreaded, Instrument, QtCore.QObject):
 
     # definition of CmdMessenger commands
     commands = [["check_arduino",""],  
-                ["ret_fast_intensity","fff"],
-                ["set_cont_signal_readout","?"],
-                ["get_cont_signal_readout",""],
+                ["ret_fast_intensity","ff"],
                 ["sweep_delay","iif"], # param: number of data points, averaged data points, delay increment in seconds
                 ["set_delay","f"],
                 ["get_delay","f"],
                 ["get_intensity_trace","iif"],
-                ["fast_ADC_read","i"],
+                ["osci_mode","i"],
                 ["ret_string","s"],
                 ["ret_int","i"],
                 ["ret_char","c"],
@@ -30,7 +30,7 @@ class ArduinoDueOptofluidics(CmdMessengerThreaded, Instrument, QtCore.QObject):
                 ["ret_bool","?"],
                 ["ret_byte","b"]]
 
-    intensity_signal = QtCore.Signal(float,float,float)
+    intensity_signal = QtCore.Signal(float,float)
 
     def __init__(self, port):
         # get Arduino board/serial instance
@@ -39,21 +39,58 @@ class ArduinoDueOptofluidics(CmdMessengerThreaded, Instrument, QtCore.QObject):
         CmdMessengerThreaded.__init__(self, self.DueBoard, self.commands)
         QtCore.QObject.__init__(self)
         self.check_arduino_response = True
+        self.incomming_data = []  
+        # open a data file and set it as "current"
+        self.data_file = datafile.current()  
+#        self.data = np.zeros(0)          
 
     def __del__(self):
+        if isinstance(self.data_file, datafile.DataFile):
+            self.data_file.close()
         self.close()
+        
+    def init_scan(self, num_points):
+        self.data_index=0
+        self.data_delay = np.zeros(num_points)
+        self.data_signal1 = np.zeros(num_points)
+        self.data_delay.fill(np.nan)
+        self.data_signal1.fill(np.nan)        
+            
+    def close_scan(self):
+        curr_scan = self.datafile_group.create_group('scan_%d')
+        curr_scan.create_dataset('delay', data=self.data_delay)
+        curr_scan.create_dataset('signal1', data=self.data_signal1)
+        
+    def osci_trace(self, num_points, description=None):
+        self.init_scan(num_points)
+        self.send("osci_mode", num_points)
+        self.datafile_group = self.data_file.require_group('oscilloscope_traces') # create new group in data file        
+#        self.curr_dataset = datafile_group.create_dataset('scan_%d', attrs=dict(description=description)) 
+#        self.curr_dataset = datafile_group.create_group('scan_%d', attrs=dict(description=description))
+
+    def delay_trace(self, num_points, num_pulses, time_increment_in_sec):
+        self.init_scan(num_points)
+        self.send("sweep_delay", num_points, num_pulses, time_increment_in_sec)
+        self.datafile_group = self.data_file.require_group('delay_traces')
+    
+    def pulse_intensity_trace(self, num_points, num_pulses, time_delay_in_sec):
+        self.init_scan(num_points)
+        self.send("get_intensity_trace", num_points, num_pulses, time_delay_in_sec)
+        self.datafile_group = self.data_file.require_group('pulse_intensity_traces')
+              
 
     def response_to_command(self, cmd_name, msg, message_time):
-#        print "cmd_name:", cmd_name
-#        print "msg:", msg
-#      if cmd_name == 'ret_intensity':  # received intensity values
-#            self.intensity_signal.emit(msg[0],msg[1])
         if cmd_name == 'ret_fast_intensity':
-#            print "msg:",msg
-            self.intensity_signal.emit(msg[0],msg[1],msg[2])
+            self.data_delay[self.data_index]=msg[1]
+            self.data_signal1[self.data_index]=msg[0]
+
+            self.intensity_signal.emit(msg[0],msg[1])
+            self.data_index+=1
+            if self.data_delay.shape[0] == self.data_index:
+                self.close_scan()
         else:
             print cmd_name, msg
-
+            
     def get_qt_ui(self):
         self.gui=ArduinoUI(self)
         return self.gui
@@ -90,15 +127,15 @@ class ArduinoUI(QtWidgets.QWidget):
         # event handling
         self.due.intensity_signal.connect(self.update_gui)
 
-    def update_gui(self,value1,value2,value3):
+    def update_gui(self,value1,value2):
         self.signal1_deque.append(value1)
-        self.signal2_deque.append(value2)
-        self.xaxis_deque.append(value3)
+#        self.signal2_deque.append(value2)
+        self.xaxis_deque.append(value2)
 #        if self.update_counter == 9:
         self.signal1_plot.clear()
         self.signal1_plot.plot(self.xaxis_deque, self.signal1_deque, symbol='o')
         self.signal2_plot.clear()
-        self.signal2_plot.plot(self.xaxis_deque, self.signal2_deque, symbol='o')
+        self.signal2_plot.plot(self.xaxis_deque, symbol='o')
         self.update_counter=0
         
     def clear(self):
